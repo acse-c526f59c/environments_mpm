295473613660586e0bb73ab6a22edc4041cbf065
acse-c526f59c <ch322@ic.ac.uk>
2022-10-10 15:39:18 +0100

    new method

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,9 +1,10 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
 from scipy import misc
+import math
 
 
-__all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve', 'math_mathod']
 
 
 def rand_array(shape):
@@ -12,5 +13,8 @@ def rand_array(shape):
 def smooth_image(a, sigma=1):
  return gaussian_filter(a, sigma=sigma)
 
- def my_mat_solve(A, b):
+def my_mat_solve(A, b):
     return A.inv()*b
+
+def math_method(x):
+    return x*math.pi
---
+++ scripts/math_method.py
@@ -0,0 +1,4 @@
+from envtest import math_method
+
+answer = math_method(3)
+print(answer)
